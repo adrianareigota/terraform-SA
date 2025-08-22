bc53f04b6f8734a939550998583047d7f589bce2
adrianareigota <adrianareigota@msn.com>
2025-08-22 08:53:36 -0300

    fixing variables in module compute instance

--- modules/compute_instance/variables.tf
+++ modules/compute_instance/variables.tf
@@ -1,7 +1,29 @@
-variable "project"       { type = string }
-variable "region"        { type = string }
-variable "zone"          { type = string }
-variable "name"          { type = string }
-variable "machine_type"  { type = string }
-variable "labels"        { type = map(string) default = {} }
-variable "network_tags"  { type = list(string) default = [] }
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "labels" {
+  type    = map(string)
+  default = {}
+}
+
+variable "network_tags" {
+  type    = list(string)
+  default = []
+}
