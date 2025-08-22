e5c7ac2f3e2155280b97dc40b9dbc8b3e60112f1
adrianareigota <33938658+adrianareigota@users.noreply.github.com>
2025-08-22 05:59:42 -0300

    Update variables.tf

--- envs/staging/variables.tf
+++ envs/staging/variables.tf
@@ -1,7 +1,29 @@
-variable "project"      { type = string }
-variable "region"       { type = string }
-variable "zone"         { type = string }
-variable "name"         { type = string }
-variable "machine_type" { type = string }
-variable "labels"       { type = map(string) }
-variable "network_tags" { type = list(string) }
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "labels" {
+  type    = map(string)
+  default = {}
+}
+
+variable "network_tags" {
+  type    = list(string)
+  default = []
+}
